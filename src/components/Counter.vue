65fc978145eca98553a1d8c42129b438cbcfa2e1
Glitch (child-task) <none>
2017-12-12 15:39:33 +0000

    null

---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>
+      {{count}}
+    </h2>
+    <div>
+      <button @click="increment">
+        +
+      </button>
+      <button @click="decrement">
+        -
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+  
+export default {
+  name: 'Counter',
+  computed: {
+    ...mapGetters([
+      'count'
+    ])
+  },
+  methods: {
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  getters: {
+    count: state => {
+      return state.count
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count += 1
+    },
+    decrement(state) {
+      state.count -= 1
+    }
+  }
+})
