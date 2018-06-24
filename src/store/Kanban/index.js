026a81f2a9db5241d5d6dcb1d5da6420542c6bf8
Glitch (child-task) <none>
2018-06-24 09:01:36 +0000

    Updated for Syohgaku grade 1

--- src/store/Kanban/index.js
+++ src/store/Kanban/index.js
@@ -19,6 +19,260 @@ export default {
         name: "おきる",
         due: {
           hours: 6,
+          minutes: 10
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 0, title: "⏰めざましどけい を とめる"},
+          {id: 1, title: "🚽トイレ に いく"},
+          {id: 2, title: "🚪ドア を しめる"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 1,
+        name: "こらしょタイム",
+        due: {
+          hours: 6,
+          minutes: 40
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 3, title: "こくご"},
+          {id: 4, title: "さんすう"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 2,
+        name: "おきがえ",
+        due: {
+          hours: 6,
+          minutes: 45
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 5, title: "👙パンツ を はく"},
+          {id: 6, title: "👕シャツ を きる"},
+          {id: 7, title: "👖ズボン を はく"},
+          {id: 8, title: "👚うわぎ を きる"},
+          {id: 9, title: "🧦くつした を はく"},
+          {id: 10, title: "📛なふだ を つける"},
+          {id: 11, title: "🤧ポシェット を つける"},
+          {id: 12, title: "せんたくもの の うらおもて を なおす"},
+          {id: 13, title: "せんたくもの を カゴ に いれる"},
+          {id: 14, title: "スリープガウン を ハンガー に かける"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 3,
+        name: "あさごはん",
+        due: {
+          hours: 7,
+          minutes: 10
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 15, title: "🥪あさごはん を たべる"},
+          {id: 16, title: "💊くすり を のむ"},
+          {id: 17, title: "🍵おちゃ を のむ"},
+          {id: 18, title: "🍽おさら を さげる"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 4,
+        name: "かおあらい",
+        due: {
+          hours: 7,
+          minutes: 15
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 19, title: "👏て を あらう"},
+          {id: 20, title: "😉かお を あらう"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 5,
+        name: "はみがき",
+        due: {
+          hours: 7,
+          minutes: 20
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 21, title: "😁じぶん はみがき"},
+          {id: 22, title: "😁しあげ はみがき"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 6,
+        name: "かみのけ",
+        due: {
+          hours: 7,
+          minutes: 25
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 23, title: "👧🏻かみ を むすぶ"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 7,
+        name: "トイレ",
+        due: {
+          hours: 7,
+          minutes: 30
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 24, title: "🚽トイレ に いく"},
+          {id: 25, title: "🚪ドア を しめる"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 8,
+        name: "おでかけ",
+        due: {
+          hours: 7,
+          minutes: 30
+        },
+        groupId: 1,
+        openedTasks: [
+          {id: 26, title: "🚶いってらっしゃい！"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 9,
+        name: "おふろ",
+        due: {
+          hours: 18,
+          minutes: 30
+        },
+        groupId: 2,
+        openedTasks: [
+          {id: 30, title: "せんたくもの の うらおもて を なおす"},
+          {id: 31, title: "せんたくもの を カゴ に いれる"},
+          {id: 32, title: "🛀おふろ に はいる"},
+          {id: 33, title: "👙パンツ を はく"},
+          {id: 34, title: "👘パジャマ を きる"},
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 10,
+        name: "よるごはん",
+        due: {
+          hours: 19,
+          minutes: 30
+        },
+        groupId: 2,
+        openedTasks: [
+          {id: 35, title: "🍖よるごはん を たべる"},
+          {id: 36, title: "💊くすり を のむ"},
+          {id: 37, title: "🍵おちゃ を のむ"},
+          {id: 38, title: "🍽おさら を さげる"},
+          {id: 39, title: "👏て を あらう"},
+          {id: 40, title: "😉かお を あらう"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 11,
+        name: "はみがき",
+        due: {
+          hours: 19,
+          minutes: 45
+        },
+        groupId: 2,
+        openedTasks: [
+          {id: 41, title: "😁じぶん はみがき"},
+          {id: 42, title: "😁しあげ はみがき"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      {
+        id: 12,
+        name: "おしたく",
+        due: {
+          hours: 20,
+          minutes: 0
+        },
+        groupId: 2,
+        openedTasks: [
+          {id: 43, title: "🥛コップ を ふくろ に いれる"},
+          {id: 44, title: "🥛コップ を かばん に いれる"},
+          {id: 45, title: "🥢はし、🥄スプーン を かばん に いれる"},
+          {id: 46, title: "ナプキン を かばん に いれる"},
+          {id: 47, title: "コマ を かばん に いれる"},
+          {id: 48, title: "なわとび を かばん に いれる"},
+          {id: 49, title: "🤧ティッシュ を ポケット に いれる"},
+          {id: 50, title: "🚰ハンカチ を ポケット に いれる"},
+          {id: 51, title: "📛バッチ を つける"},
+          {id: 52, title: "🎗️リボン を つける"},
+          {id: 53, title: "🎒おきがえぶくろ を よういする"},
+          {id: 54, title: "📚えほんぶくろ を よういする"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      },
+      
+      {
+        id: 13,
+        name: "ねる",
+        due: {
+          hours: 20,
+          minutes: 30
+        },
+        groupId: 2,
+        openedTasks: [
+          {id: 55, title: "👘スリープガウン を きる"},
+          {id: 56, title: "🛏️おやすみなさい♪"}
+        ],
+        workingTasks: [],
+        closedTasks: [],
+        wontTasks: []
+      }
+    ]
+    /* 幼稚園
+    stories: [
+      {
+        id: 0,
+        name: "おきる",
+        due: {
+          hours: 6,
           minutes: 40
         },
         groupId: 1,
@@ -253,6 +507,7 @@ export default {
         wontTasks: []
       }
     ]        
+    */
   },
   getters: {
     numStories: state => (group_id) => {
